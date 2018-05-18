848b8250020e55608aa43c229a5cc3fb19d04c4b
Raja Jawahar <rajajawahar77@gmail.com>
2018-05-18 19:22:03 +0530

    Progrees Dialogue created through Anko

--- app/src/main/java/com/sample/tryanko/MainActivity.kt
+++ app/src/main/java/com/sample/tryanko/MainActivity.kt
@@ -2,23 +2,28 @@ package com.sample.tryanko
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.alert_dialogue
 import kotlinx.android.synthetic.main.activity_main.button
 import kotlinx.android.synthetic.main.activity_main.coordinatorLayout
 import kotlinx.android.synthetic.main.activity_main.dialogue
 import kotlinx.android.synthetic.main.activity_main.longToast
 import kotlinx.android.synthetic.main.activity_main.longsnackbar
+import kotlinx.android.synthetic.main.activity_main.progress_dialogue
 import kotlinx.android.synthetic.main.activity_main.snackbar
+import org.jetbrains.anko.alert
 import org.jetbrains.anko.design.longSnackbar
 import org.jetbrains.anko.design.snackbar
 import org.jetbrains.anko.longToast
+import org.jetbrains.anko.progressDialog
 import org.jetbrains.anko.selector
 import org.jetbrains.anko.toast
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
 
     button.setOnClickListener {
       toast("This is a toast")
@@ -37,6 +42,22 @@ class MainActivity : AppCompatActivity() {
       selector("Select an option", options) { dialogInterface, i ->
         toast("You selected ${options[i]}")
       }
+    }
+    alert_dialogue.setOnClickListener {
+
+      alert(message = "Are you want to exit the application", title = "LogOut") {
+        positiveButton("Proceed") {
+          toast("You clicked Proceed")
+        }
+        negativeButton("Cancel") {
+          toast("You clicked Cancel")
+        }
+      }.show()
+    }
+    progress_dialogue.setOnClickListener {
+      progressDialog(message = "Fetching Data", title = "Loading") {
+
+      }.show()
     }
 
   }
